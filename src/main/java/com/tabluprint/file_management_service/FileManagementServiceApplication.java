a69bc7ccaa690c70b4d9479c7c62ff1b9eb07514
Doan Pham <doannphamm57@gmail.com>
2024-06-23 14:20:16 +0700

    init

---
+++ src/main/java/com/tabluprint/file_management_service/FileManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tabluprint.file_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementServiceApplication.class, args);
+	}
+
+}
