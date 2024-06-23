ea1375792cdef259386351dfb47e4d1eee551cf3
Doan Pham <doannphamm57@gmail.com>
2024-06-23 15:32:13 +0700

    feat: add endpoint for extracting data to Excel

--- src/main/java/com/tabluprint/file_management_service/FileManagementServiceApplication.java
+++ src/main/java/com/tabluprint/file_management_service/FileManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.tabluprint.file_management_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
 public class FileManagementServiceApplication {
 
 	public static void main(String[] args) {
